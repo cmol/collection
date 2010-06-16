875b072abf3e7ba3ab749d45726cb713cb6d635e
Claus Lensbøl <cmol@cmol.dk>
2010-06-16 10:39:24 +0200

    Fixed problem with saving imdb id

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,3 @@
 class Movie < ActiveRecord::Base
-  attr_accessible :title, :rating, :duration, :directed_by, :cast, :genre, :plot, :poster_url, :imdbid, :user_id, :media_id, :loaned_to
+  attr_accessible :title, :rating, :duration, :directed_by, :cast, :genre, :plot, :poster_url, :imdb, :user_id, :media_id, :loaned_to
 end
