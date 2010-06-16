c5c5cfe3ecfcbc04210973c2374a96fa0fcf2e98
Claus Lensbøl <cmol@cmol.dk>
2010-06-16 11:28:31 +0200

    Added authlogic to gem list

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,7 @@ Rails::Initializer.run do |config|
   config.gem "imdb"
   config.gem "formtastic"
   config.gem "jrails"
+  config.gem "authlogic"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
