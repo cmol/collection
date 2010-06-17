7018ab7d2caba5e24440a3593240ffec7209feec
Claus Lensbøl <cmol@cmol.dk>
2010-06-17 11:24:16 +0200

    Added admin bolean to users table

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100616131439) do
+ActiveRecord::Schema.define(:version => 20100617091824) do
 
   create_table "movies", :force => true do |t|
     t.string   "title"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(:version => 20100616131439) do
     t.boolean  "active",             :default => false, :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
 end
