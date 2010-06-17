3fe6c5bea97120e5085f0dfbc903b886dbda15ce
Claus Lensbøl <cmol@cmol.dk>
2010-06-17 12:27:16 +0200

    Added year to the movies and implemented it. I had forgot....

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
-  attr_accessible :title, :rating, :duration, :directed_by, :cast, :genre, :plot, :poster_url, :imdb, :user_id, :media_id, :loaned_to
+  attr_accessible :title, :year, :rating, :duration, :directed_by, :cast, :genre, :plot, :poster_url, :imdb, :user_id, :media_id, :loaned_to
   belongs_to :user
   belongs_to :media
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100617094852) do
+ActiveRecord::Schema.define(:version => 20100617102045) do
 
   create_table "medias", :force => true do |t|
     t.string   "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20100617094852) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "imdb"
+    t.integer  "year"
   end
 
   create_table "users", :force => true do |t|
