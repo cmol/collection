4dd234b1037e1a29498d3c1cd64327a0fa2c13fd
Claus Lensbøl <cmol@cmol.dk>
2010-06-18 14:32:24 +0200

    Added validation to some of the movie fields

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,5 @@ class Movie < ActiveRecord::Base
   attr_accessible :title, :year, :rating, :duration, :directed_by, :cast, :genre, :plot, :poster_url, :imdb, :user_id, :media_id, :loaned_to
   belongs_to :user
   belongs_to :media
+  validates_presence_of :title, :media_id, :user_id
 end
