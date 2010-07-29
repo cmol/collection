6071735274921caa1bea5eeac2ceae8425d144b7
Claus Lensbøl <cmol@cmol.dk>
2010-07-29 15:03:59 +0200

    Added will paginate to medias

--- config/environment.rb
+++ config/environment.rb
@@ -23,6 +23,7 @@ Rails::Initializer.run do |config|
   config.gem "formtastic"
   config.gem "jrails"
   config.gem "authlogic"
+  config.gem "will_paginate"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
