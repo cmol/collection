79a33fff6b52f8924ba4b9be1e1d9570bd65dac8
Claus Lensbøl <cmol@cmol.dk>
2010-07-29 10:29:31 +0200

    Added wishlist to attr_accessible

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
-  attr_accessible :title, :year, :rating, :duration, :directed_by, :cast, :genre, :plot, :poster_url, :imdb, :user_id, :media_id, :loaned_to
+  attr_accessible :title, :year, :rating, :duration, :directed_by, :cast, :genre, :plot, :poster_url, :imdb, :user_id, :media_id, :loaned_to, :wishlist
   belongs_to :user
   belongs_to :media
   validates_presence_of :title, :media_id, :user_id
